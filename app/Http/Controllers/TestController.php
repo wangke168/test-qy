87faa63727c5b991cf1778ff1dde7153c58be4f1
wangke168 <wangke168@gmail.com>
2018-06-21 04:51:29 +0000

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use EasyWeChat\Factory;
+use EasyWeChat\Kernel\Messages\Text;
 class TestController extends Controller
 {
-    //
+    public function index()
+    {
+        $config = [
+            'corp_id' => 'wwfb1970349326c73f',
+
+            'agent_id' => 1000004,
+            'secret' => 'TsbKy9F_yo_d3bXKJ0HNqgcq4FjXW3dPXmXLhyVm918',
+
+            // server config
+            'token' => 'jianpiao',
+            'aes_key' => 'X5HFXA537wZkVwUicueeuPlsGgvgftDPdyv9pnNMaMp',
+
+            //...
+        ];
+        $app = Factory::work($config);
+        $message = new Text('FromUserName');
+        $result = $app->customer_service->message($message)->to('hd_wangke')->send();
+
+    }
 }
