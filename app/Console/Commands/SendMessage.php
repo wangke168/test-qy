43e210b5448523122de041b186d6572008e5d494
wangke168 <wangke168@gmail.com>
2018-08-14 15:54:36 +0800

    test

--- app/Console/Commands/SendMessage.php
+++ app/Console/Commands/SendMessage.php
@@ -41,7 +41,7 @@ class SendMessage extends Command
     {
 
 
-        $config=[
+     /*   $config=[
             'corp_id' => 'wwfb1970349326c73f',
             'agent_id' => 1000004,
             'secret' => 'TsbKy9F_yo_d3bXKJ0HNqgcq4FjXW3dPXmXLhyVm918',
@@ -51,33 +51,63 @@ class SendMessage extends Command
         $weObj=Factory::work($config);
 
         $weObj->server->push(function(){
-            $today=Carbon::now()->toDateString();
-//        return $today;
-            $url = "http://10.0.61.202/CheckSectionsTurnover.aspx?startdate=".$today."&enddate=".$today;
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-            $json = curl_exec($ch);
-            $data = json_decode($json, true);
+        $today=Carbon::now()->toDateString();
+        $url = "http://10.0.61.202/CheckSectionsTurnover.aspx?startdate=".$today."&enddate=".$today;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        $json = curl_exec($ch);
+        $data = json_decode($json, true);
 
 
-            $str=$today."数据如下\n";
-            $str=$str.$data['resultList'][0]['section']."\n";
-            $str=$str."营收:".round($data['resultList'][0]['turnover'],2)."元\n";
-            $str=$str."人次:".$data['resultList'][0]['personTime']."\n\n";
-            $str=$str.$data['resultList'][1]['section']."\n";
-            $str=$str."营收:".round($data['resultList'][1]['turnover'],2)."元\n";
-            $str=$str."人次:".$data['resultList'][1]['personTime']."\n\n";
-            $str=$str.$data['resultList'][2]['section']."\n";
-            $str=$str."营收:".round($data['resultList'][2]['turnover'],2)."元\n";
-            $str=$str."人次:".$data['resultList'][2]['personTime']."\n\n";
-            return $str;
-        });
+        $str=$today."数据如下\n";
+        $str=$str.$data['resultList'][0]['section']."\n";
+        $str=$str."营收:".round($data['resultList'][0]['turnover'],2)."元\n";
+        $str=$str."人次:".$data['resultList'][0]['personTime']."\n\n";
+        $str=$str.$data['resultList'][1]['section']."\n";
+        $str=$str."营收:".round($data['resultList'][1]['turnover'],2)."元\n";
+        $str=$str."人次:".$data['resultList'][1]['personTime']."\n\n";
+        $str=$str.$data['resultList'][2]['section']."\n";
+        $str=$str."营收:".round($data['resultList'][2]['turnover'],2)."元\n";
+        $str=$str."人次:".$data['resultList'][2]['personTime']."\n\n";
+        return $str;
 
+    });*/
+        $options = array(
+            'token' => "test",    //填写应用接口的Token
+            'encodingaeskey' => "uY5rLOibklJSaHt8suAz861k7jQdUc8a0vrv4crvNq8",//填写加密用的EncodingAESKey
+            'appid' => "wwfb1970349326c73f",   //填写高级调用功能的appid
+            'appsecret' => "TsbKy9F_yo_d3bXKJ0HNqgcq4FjXW3dPXmXLhyVm918",
+            'debug' => env('QY_WECHAT_DEBUG', 'debug'),
+            'logcallback' => env('QY_WECHAT_LOGCALLBACK', 'logcallback'),
+            'agentid' => "1000004", //应用的id
+        );
+        $weObj = new \Wechat($options);
+        $date = array(
+            'touser' => 'hd_wangke',
+            "toparty" => "",
+            "totag" => "",
+            'agentid' => '1000004',    //应用id
+            'msgtype' => 'news',  //根据信息类型，选择下面对应的信息结构体
+
+            "news" => array(            //不支持保密
+                "articles" => [
+                    array(
+                        "title" => "title",
+                        "description" => "description",
+                        "url" => "http://weix2.hengdianworld.com/enterprise/article/articledetail_ldjl.php?date=".date("Y-m-d"),
+                        "picurl" => "https://weix.hengdianworld.com/images/ldjl_data.jpg",
+                    ),
+                ]
+            ),
+
+        );
+        $weObj->sendMessage($date);
+/*
         $response = $weObj->server->serve();
 
-        return $response;
+        return $response;*/
 
 
 
