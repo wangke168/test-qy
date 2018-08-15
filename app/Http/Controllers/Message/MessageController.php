1563d096353a46a7a431b895f0c7e0e5fe35ac08
wangke168 <wangke168@gmail.com>
2018-08-15 16:18:57 +0800

    test

--- app/Http/Controllers/Message/MessageController.php
+++ app/Http/Controllers/Message/MessageController.php
@@ -31,33 +31,17 @@ class MessageController extends Controller
 
         $this->weObj->server->push(function ($message) {
 
-/*            $today = Carbon::now()->toDateString();
-            $url = env('YDPT_URL', 'url');
-            $url = $url."CheckSectionsTurnover.aspx?startdate=" . $today . "&enddate=" . $today;
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-            $json = curl_exec($ch);
-            $data = json_decode($json, true);
+            switch ($message['MsgType']) {
+                case 'text':
 
-
-            $str = $today . "数据如下\n";
-            $str = $str . $data['resultList'][0]['section'] . "\n";
-            $str = $str . "营收:" . round($data['resultList'][0]['turnover'], 2) . "元\n";
-            $str = $str . "人次:" . $data['resultList'][0]['personTime'] . "\n\n";
-            $str = $str . $data['resultList'][1]['section'] . "\n";
-            $str = $str . "营收:" . round($data['resultList'][1]['turnover'], 2) . "元\n";
-            $str = $str . "人次:" . $data['resultList'][1]['personTime'] . "\n\n";
-            $str = $str . $data['resultList'][2]['section'] . "\n";
-            $str = $str . "营收:" . round($data['resultList'][2]['turnover'], 2) . "元\n";
-            $str = $str . "人次:" . $data['resultList'][2]['personTime'] . "\n\n";*/
-            return $this->Message();
-
+                    return 'sadas';
+                    break;
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
-
         $response = $this->weObj->server->serve();
-
         return $response;
     }
 
