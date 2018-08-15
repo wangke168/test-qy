de35d6ff40f9c64457fd48fcbb91f4a2d1044ca9
wangke168 <wangke168@gmail.com>
2018-08-15 16:53:53 +0800

    test

--- app/Console/Commands/SendMessage.php
+++ app/Console/Commands/SendMessage.php
@@ -40,60 +40,8 @@ class SendMessage extends Command
     public function handle()
     {
 
-
-     /*   $config=[
-            'corp_id' => 'wwfb1970349326c73f',
-            'agent_id' => 1000004,
-            'secret' => 'TsbKy9F_yo_d3bXKJ0HNqgcq4FjXW3dPXmXLhyVm918',
-            'token' => 'test',
-            'aes_key' => 'uY5rLOibklJSaHt8suAz861k7jQdUc8a0vrv4crvNq8',
-        ];
-        $weObj=Factory::work($config);
-
-        $weObj->server->push(function(){
-        $today=Carbon::now()->toDateString();
-        $url = "http://10.0.61.202/CheckSectionsTurnover.aspx?startdate=".$today."&enddate=".$today;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-        $json = curl_exec($ch);
-        $data = json_decode($json, true);
-
-
-        $str=$today."数据如下\n";
-        $str=$str.$data['resultList'][0]['section']."\n";
-        $str=$str."营收:".round($data['resultList'][0]['turnover'],2)."元\n";
-        $str=$str."人次:".$data['resultList'][0]['personTime']."\n\n";
-        $str=$str.$data['resultList'][1]['section']."\n";
-        $str=$str."营收:".round($data['resultList'][1]['turnover'],2)."元\n";
-        $str=$str."人次:".$data['resultList'][1]['personTime']."\n\n";
-        $str=$str.$data['resultList'][2]['section']."\n";
-        $str=$str."营收:".round($data['resultList'][2]['turnover'],2)."元\n";
-        $str=$str."人次:".$data['resultList'][2]['personTime']."\n\n";
-        return $str;
-
-    });*/
-        $config=[
-            'corp_id' => 'wwfb1970349326c73f',
-            'agent_id' => 1000004,
-            'secret' => 'TsbKy9F_yo_d3bXKJ0HNqgcq4FjXW3dPXmXLhyVm918',
-            'token' => 'test',
-            'aes_key' => 'uY5rLOibklJSaHt8suAz861k7jQdUc8a0vrv4crvNq8',
-        ];
-        $weObj=Factory::work($config);
-        $accessToken = $weObj->access_token;
-        $token = $accessToken->getToken(); // token 数组  token['access_token'] 字符串
-        $msg=$this->message();
-        $url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" . $token['access_token'];
-        $data="{\"touser\":\"hd_wangke\",\"msgtype\":\"text\",\"agentid\":1000004,\"text\":{\"content\":\"asdasdas\"},\"safe\":0}";
-        $res = $this->curlPost($url,$data);
-/*
-        $response = $weObj->server->serve();
-
-        return $response;*/
-
-
+        $url = "https://" . $_SERVER['HTTP_HOST'] . "/sendmessage";
+        $this->curlPost($url);
 
     }
     private function curlPost($url,$data="")
