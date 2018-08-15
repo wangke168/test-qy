fffb3423eb9559f54b6f96878012e8bb3ad08e21
wangke168 <wangke168@gmail.com>
2018-08-15 15:29:32 +0800

    test

--- app/Console/Commands/SendMessage.php
+++ app/Console/Commands/SendMessage.php
@@ -96,26 +96,22 @@ class SendMessage extends Command
 
 
     }
-    private function curlPost($url,$data=""){
-        $ch = curl_init();
-        $opt = array(
-            CURLOPT_URL     => $url,
-            CURLOPT_HEADER  => 0,
-            CURLOPT_POST    => 1,
-            CURLOPT_POSTFIELDS      => $data,
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_TIMEOUT         => 20
-        );
-        $ssl = substr($url,0,8) == "https://" ? TRUE : FALSE;
-        if ($ssl){
-            $opt[CURLOPT_SSL_VERIFYHOST] = 2;
-            $opt[CURLOPT_SSL_VERIFYPEER] = FALSE;
+    private function curlPost($url,$data="")
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)){
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         }
-        curl_setopt_array($ch,$opt);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
     }
+
     private  function  message()
     {
         $today=Carbon::now()->toDateString();
