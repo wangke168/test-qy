01a38cacbee0dc4a90ed1e73c211588b0b627a31
wangke168 <wangke168@gmail.com>
2018-08-16 16:00:56 +0800

    test

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'test','jianpiao','tglm','cardcreate','cardquery','sendmessage/*',
+        'test','jianpiao','tglm','cardcreate','cardquery','message','sendmessage/*',
     ];
 }
