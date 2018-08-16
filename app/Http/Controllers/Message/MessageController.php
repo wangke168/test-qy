f1580fd33958d60407612f95c4d39bc341b952fb
wangke168 <wangke168@gmail.com>
2018-08-16 15:47:45 +0800

    test

--- app/Http/Controllers/Message/MessageController.php
+++ app/Http/Controllers/Message/MessageController.php
@@ -55,11 +55,22 @@ class MessageController extends Controller
     public function SendMessage()
     {
 
-        $accessToken = $this->weObj->access_token;
+        $config = [
+            'corp_id' => 'wwfb1970349326c73f',
+
+            'agent_id' => 1000009,
+            'secret' => 'kEJJDuCTuSXwf6DyAXFxee1VnNFC5HfEpldCkMRqs9o',
+
+            // server config
+            'token' => 'message',
+            'aes_key' => 'JGDBtwV7jgujnJbbfKC1DOEExK7al8lFTM5GkUeLCsI',
+        ];
+        $weObj = Factory::work($config);
+        $accessToken = $weObj->access_token;
         $token = $accessToken->getToken(); // token 数组  token['access_token'] 字符串
         $msg = $this->message();
         $url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" . $token['access_token'];
-        $data = "{\"touser\":\"hd_wangke\",\"msgtype\":\"text\",\"agentid\":1000009,\"text\":{\"content\":\"$msg\"},\"safe\":0}";
+        $data = "{\"touser\":\"hd_wangke\",\"msgtype\":\"text\",\"agentid\":1000009,\"text\":{\"content\":\"asdasdas\"},\"safe\":0}";
         $this->curlPost($url, $data);
 
     }
