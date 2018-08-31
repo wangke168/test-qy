658d97894c3f6d301a21638bc99b922019158c8c
wangke168 <wangke168@gmail.com>
2018-08-31 14:14:22 +0800

    test

--- app/Http/Controllers/Query/QueryController.php
+++ app/Http/Controllers/Query/QueryController.php
@@ -6,20 +6,22 @@ use App\Http\Controllers\Controller;
 use EasyWeChat\Factory;
 use EasyWeChat\Kernel\Messages\News;
 use EasyWeChat\Kernel\Messages\NewsItem;
+
 class QueryController extends Controller
 {
     public $weObj;
     public $config;
+
     public function __construct()
     {
-        $this->config=[
+        $this->config = [
             'corp_id' => env('QY_WECHAT_APPID', 'corp_id'),
             'agent_id' => env('QY_WECHAT_QUERY_AGENTID', 'agent_id'),
             'secret' => env('QY_WECHAT_QUERY_APPSECRET', 'secret'),
             'token' => env('QY_WECHAT_QUERY_TOEKN', 'token'),
             'aes_key' => env('QY_WECHAT_QUERY_ENCODINGAESKEY', 'aes_key'),
         ];
-        $this->weObj=Factory::work($this->config);
+        $this->weObj = Factory::work($this->config);
     }
 
     public function index()
@@ -29,7 +31,7 @@ class QueryController extends Controller
 
             switch ($message['MsgType']) {
                 case 'text':
-                    $news=$this->Check_tecket($message['Content']);
+                    $news = $this->Check_tecket($message['Content']);
                     return $news;
                     break;
                 default:
@@ -46,31 +48,20 @@ class QueryController extends Controller
     {
 
         $url = env('YDPT_URL', 'url');
-        $url = $url ."SearechOrderUseDetails.aspx?password=".$password;
+        $url = $url . "SearechOrderUseDetails.aspx?password=" . $password;
         $data = $this->curl($url);
 
-        if ($data['viewSpotName']==null)
-        {
-            $str= "该识别码七天内无订单";
-        }
-        else{
+        if ($data['viewSpotName'] == null) {
+            $str = "该识别码七天内无订单";
+        } else {
             $str = "门票种类：" . $data['viewSpotName'];
 
-            if ($data['isUse'])
-            {
-                $isUse="已使用";
-            }
-            else
-            {
-                $isUse="未使用";
-            }
-
-            $str = $str."\n\n使用情况：" . $isUse;
+            $str = $str . "\n\n使用情况：" . $data['orderStatus'];
 
             $count = count($data['playedViewSpot']);
             for ($x = 0; $x < $count; $x++) {
-                $str=$str."\n\n已检景点:".$data['playedViewSpot'][$x]['playedViewSpotName'];
-                $str=$str."\n\n检票时间:".$data['playedViewSpot'][$x]['playedTime'];
+                $str = $str . "\n\n已检景点:" . $data['playedViewSpot'][$x]['playedViewSpotName'];
+                $str = $str . "\n\n检票时间:" . $data['playedViewSpot'][$x]['playedTime'];
             }
 //            echo $data['unPlayedViewSpot'][0]['unPlayedViewSpotName'].'<br>';
         }
