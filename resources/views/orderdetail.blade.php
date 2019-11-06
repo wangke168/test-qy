c25fdc0546430a554e76e0c4cb5a5c104ced123e
wangke168 <wangke168@gmail.com>
2019-11-06 15:14:08 +0800

    test

--- resources/views/orderdetail.blade.php
+++ resources/views/orderdetail.blade.php
@@ -26,7 +26,7 @@
         }
 
         .flex-center {
-            align-items: center;
+            align-items: start;
             display: flex;
             justify-content: center;
         }
@@ -65,7 +65,7 @@
     </style>
 </head>
 <body>
-<div class="position-ref full-height">
+<div class="flex-center position-ref full-height">
 
     <div class="content">
             {!! $info !!}
